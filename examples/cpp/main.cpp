ef283e460e51742ed0a4ea75dd95ce08db16efbc
Yiheng <development.henryquan@gmail.com>
2023-08-13 19:41:46 +1000

    updated example to read for input

--- examples/cpp/main.cpp
+++ examples/cpp/main.cpp
@@ -1,10 +1,21 @@
-#include <iostream>
 #include "reader.h"
+#include <iostream>
 
-int main() {
-    const auto reader = examples::Reader::from_file("personal_rating.json");
-    auto result = reader->get_value("123");
-    const auto& result_string = kt_lib.model.RemoteExpectValue.toString(result);
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    std::cout << "Usage: main.exe <key>" << std::endl;
+    return 1;
+  }
+
+  const auto reader = examples::Reader::from_file("personal_rating.json");
+  auto key = argv[1];
+  auto result = reader->get_value(key);
+  if (result.pinned == nullptr) {
+    std::cout << "No such key" << std::endl;
+  } else {
+    const auto &result_string = kt_lib.model.RemoteExpectValue.toString(result);
     std::cout << result_string << std::endl;
-    return 0;
+  }
+
+  return 0;
 }
